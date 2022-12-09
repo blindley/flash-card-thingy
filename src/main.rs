3b0f45c40fdb7c5bfeab18f4a00cd6ea10bceb40
Benjamin Lindley <benjameslindley@gmail.com>
2022-12-08 22:35:51 -0600

    loading content from external html file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 #[macro_use] extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
-
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index])
+}
+
+#[get("/")]
+fn index() -> String {
+    let p = "data/templates/basic.html";
+    std::fs::read_to_string(p)
+        .unwrap_or_else(|e| format!("Error loading '{p}' : {e}"))
 }
